MultiCraftProject/MultiCraft

commit 12b714424d92d35fe7f5cdd25659e49b0fed2eec

iOS: remove redis, minimap and add pause texture. Fix build script

build/iOS/deps/freetype.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 . ../sdk.sh
+IPHONEOS_DEPLOYMENT_TARGET=
 FREETYPE_VERSION=2.7
 
 if [ ! -d freetype-src ]; then

build/iOS/sdk.sh
@@ -4,7 +4,7 @@
 sdk=iphoneos
 
 export IOS_COMPILER=$(xcrun --sdk $sdk --find clang)
+export IPHONEOS_DEPLOYMENT_TARGET=8.0
 export IOS_CC=$IOS_COMPILER
 export IOS_CXX=$IOS_COMPILER
-export IOS_FLAGS="-isysroot $(xcrun --sdk $sdk --show-sdk-path) -arch armv7 -arch arm64"
-
+export IOS_FLAGS="-isysroot $(xcrun --sdk $sdk --show-sdk-path) -arch armv7 -arch arm64 -Ofast -fvisibility=hidden"

build/iOS/MultiCraft/MultiCraft.xcodeproj/project.pbxproj
@@ -35,7 +35,6 @@
 		F8E6C6051DCA3F9900F64426 /* craftdef.cpp in Sources */ = {isa = PBXBuildFile; fileRef = F8E6C5211DCA3F9900F64426 /* craftdef.cpp */; };
 		F8E6C6061DCA3F9900F64426 /* database-dummy.cpp in Sources */ = {isa = PBXBuildFile; fileRef = F8E6C5231DCA3F9900F64426 /* database-dummy.cpp */; };
 		F8E6C6071DCA3F9900F64426 /* database-leveldb.cpp in Sources */ = {isa = PBXBuildFile; fileRef = F8E6C5251DCA3F9900F64426 /* database-leveldb.cpp */; };
-		F8E6C6081DCA3F9900F64426 /* database-redis.cpp in Sources */ = {isa = PBXBuildFile; fileRef = F8E6C5271DCA3F9900F64426 /* database-redis.cpp */; };
 		F8E6C6091DCA3F9900F64426 /* database-sqlite3.cpp in Sources */ = {isa = PBXBuildFile; fileRef = F8E6C5291DCA3F9900F64426 /* database-sqlite3.cpp */; };
 		F8E6C60A1DCA3F9900F64426 /* database.cpp in Sources */ = {isa = PBXBuildFile; fileRef = F8E6C52B1DCA3F9900F64426 /* database.cpp */; };
 		F8E6C60B1DCA3F9900F64426 /* debug.cpp in Sources */ = {isa = PBXBuildFile; fileRef = F8E6C52E1DCA3F9900F64426 /* debug.cpp */; };
@@ -270,8 +269,6 @@
 		F8E6C5241DCA3F9900F64426 /* database-dummy.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = "database-dummy.h"; path = "../../../../src/database-dummy.h"; sourceTree = "<group>"; };
 		F8E6C5251DCA3F9900F64426 /* database-leveldb.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = "database-leveldb.cpp"; path = "../../../../src/database-leveldb.cpp"; sourceTree = "<group>"; };
 		F8E6C5261DCA3F9900F64426 /* database-leveldb.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = "database-leveldb.h"; path = "../../../../src/database-leveldb.h"; sourceTree = "<group>"; };
-		F8E6C5271DCA3F9900F64426 /* database-redis.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = "database-redis.cpp"; path = "../../../../src/database-redis.cpp"; sourceTree = "<group>"; };
-		F8E6C5281DCA3F9900F64426 /* database-redis.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = "database-redis.h"; path = "../../../../src/database-redis.h"; sourceTree = "<group>"; };
 		F8E6C5291DCA3F9900F64426 /* database-sqlite3.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = "database-sqlite3.cpp"; path = "../../../../src/database-sqlite3.cpp"; sourceTree = "<group>"; };
 		F8E6C52A1DCA3F9900F64426 /* database-sqlite3.h */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.c.h; name = "database-sqlite3.h"; path = "../../../../src/database-sqlite3.h"; sourceTree = "<group>"; };
 		F8E6C52B1DCA3F9900F64426 /* database.cpp */ = {isa = PBXFileReference; fileEncoding = 4; lastKnownFileType = sourcecode.cpp.cpp; name = database.cpp; path = ../../../../src/database.cpp; sourceTree = "<group>"; };
@@ -766,8 +763,6 @@
 				F8E6C5241DCA3F9900F64426 /* database-dummy.h */,
 				F8E6C5251DCA3F9900F64426 /* database-leveldb.cpp */,
 				F8E6C5261DCA3F9900F64426 /* database-leveldb.h */,
-				F8E6C5271DCA3F9900F64426 /* database-redis.cpp */,
-				F8E6C5281DCA3F9900F64426 /* database-redis.h */,
 				F8E6C5291DCA3F9900F64426 /* database-sqlite3.cpp */,
 				F8E6C52A1DCA3F9900F64426 /* database-sqlite3.h */,
 				F8E6C52B1DCA3F9900F64426 /* database.cpp */,
@@ -1395,7 +1390,6 @@
 				F8E6C6DF1DCA413A00F64426 /* clientpackethandler.cpp in Sources */,
 				F8E6C6261DCA3F9900F64426 /* itemdef.cpp in Sources */,
 				F8E6C61B1DCA3F9900F64426 /* guiKeyChangeMenu.cpp in Sources */,
-				F8E6C6081DCA3F9900F64426 /* database-redis.cpp in Sources */,
 				F8E6C7671DCA423C00F64426 /* thread.cpp in Sources */,
 				F8E6C6481DCA3F9900F64426 /* pathfinder.cpp in Sources */,
 				F8E6C6531DCA3F9900F64426 /* serverobject.cpp in Sources */,
@@ -1689,6 +1683,17 @@
 				INFOPLIST_FILE = MultiCraft/Info.plist;
 				IPHONEOS_DEPLOYMENT_TARGET = 8.0;
 				LD_RUNPATH_SEARCH_PATHS = "$(inherited) @executable_path/Frameworks";
+				OTHER_CFLAGS = (
+					"$(inherited)",
+					"-isystem",
+					"\"${PODS_ROOT}/Headers/Public\"",
+					"-isystem",
+					"\"${PODS_ROOT}/Headers/Public/SDVersion\"",
+					"-isystem",
+					"\"${PODS_ROOT}/Headers/Public/SSZipArchive\"",
+					"-flto",
+					"-fvisibility=hidden",
+				);
 				OTHER_LDFLAGS = (
 					"$(inherited)",
 					"-L${SRCROOT}/../deps/irrlicht",
@@ -1745,6 +1750,17 @@
 				INFOPLIST_FILE = MultiCraft/Info.plist;
 				IPHONEOS_DEPLOYMENT_TARGET = 8.0;
 				LD_RUNPATH_SEARCH_PATHS = "$(inherited) @executable_path/Frameworks";
+				OTHER_CFLAGS = (
+					"$(inherited)",
+					"-isystem",
+					"\"${PODS_ROOT}/Headers/Public\"",
+					"-isystem",
+					"\"${PODS_ROOT}/Headers/Public/SDVersion\"",
+					"-isystem",
+					"\"${PODS_ROOT}/Headers/Public/SSZipArchive\"",
+					"-flto",
+					"-fvisibility=hidden",
+				);
 				OTHER_LDFLAGS = (
 					"$(inherited)",
 					"-L${SRCROOT}/../deps/irrlicht",

build/iOS/MultiCraft/MultiCraft/Info.plist
@@ -17,9 +17,9 @@
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>1.0</string>
+	<string>1.0.1</string>
 	<key>CFBundleVersion</key>
-	<string>2</string>
+	<string>3</string>
 	<key>LSRequiresIPhoneOS</key>
 	<true/>
 	<key>UILaunchStoryboardName</key>

build/iOS/Start.sh
@@ -14,6 +14,11 @@ echo "Creating Assets:"
 ./assets.sh
 
 echo
+echo "Creating App Icon:"
+
+./appicon.sh
+
+echo
 echo "Install CocoaPods:"
 
 pod install

build/iOS/appicon.sh
@@ -10,3 +10,5 @@ for sz in $SIZES; do
 	echo "Creating ${sz}x${sz} icon"
 	convert -resize ${sz}x${sz} $SRCFILE $DSTDIR/AppIcon-${sz}.png
 done
+
+echo "App Icon create successful"

build/iOS/deps/irrlicht.sh
@@ -16,6 +16,7 @@ fi
 
 cd source/Irrlicht
 xcodebuild build \
+	-configuration Release \
 	-project Irrlicht.xcodeproj \
 	-scheme Irrlicht_iOS \
 	-destination generic/platform=iOS

builtin/mainmenu/init.lua
@@ -97,8 +97,11 @@ local function init_globals()
 
 	tv_main:set_autosave_tab(true)
 	tv_main:add(tabs.singleplayer)
+	
+if PLATFORM ~= "iOS" then
 	tv_main:add(tabs.multiplayer)
 	tv_main:add(tabs.server)
+end
 
 	if not use_simple_menu then
 		tv_main:add(tabs.settings)
@@ -131,4 +134,3 @@ local function init_globals()
 end
 
 init_globals()
-

commit 620b4b7d6bc63b7f20eaee3a9f094b69e73f5f7e

Fix build problems with LuaJIT
(was caused by 12b714424d92d35fe7f5cdd25659e49b0fed2eec)

build/iOS/deps/freetype.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
 . ../sdk.sh
-IPHONEOS_DEPLOYMENT_TARGET=
 FREETYPE_VERSION=2.7
 
 if [ ! -d freetype-src ]; then

build/iOS/sdk.sh
@@ -2,9 +2,9 @@
 
 # This file sets the appropiate compiler and flags for compiling for iOS without XCode
 sdk=iphoneos
+osver=8.0
 
 export IOS_COMPILER=$(xcrun --sdk $sdk --find clang)
-export IPHONEOS_DEPLOYMENT_TARGET=8.0
 export IOS_CC=$IOS_COMPILER
 export IOS_CXX=$IOS_COMPILER
-export IOS_FLAGS="-isysroot $(xcrun --sdk $sdk --show-sdk-path) -arch armv7 -arch arm64 -Ofast -fvisibility=hidden"
+export IOS_FLAGS="-isysroot $(xcrun --sdk $sdk --show-sdk-path) -arch armv7 -arch arm64 -miphoneos-version-min=$osver -Ofast -fvisibility=hidden"